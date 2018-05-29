f43ca88c698c46b49abedcb2223dba1ed1e7d86c
= <bjh512512@gmail.com>
2018-05-29 19:02:21 +0900

    이 정도로..

--- ics-crawling.py
+++ ics-crawling.py
@@ -1,4 +1,4 @@
-import requests
+import requests, re
 from bs4 import BeautifulSoup
 import json
 
@@ -17,16 +17,37 @@ for i in range(0,35):
 	for j in range(len(vul)):
 		if j%2==1:
 			print(vul[j-1].text)
-			req_detail = requests.get('https://ics-cert.us-cert.gov/advisories/'+vul[j-1].text)
+			try:
+				int(vul[j-1].text[-1])
+				req_detail = requests.get('https://ics-cert.us-cert.gov/advisories/'+vul[j-1].text)	
+			except:
+				req_detail = requests.get('https://ics-cert.us-cert.gov/advisories/'+vul[j-1].text[:-1])
+			
 			html_detail = req_detail.text
 			soup_detail = BeautifulSoup(html_detail, 'html.parser')
 
 			li[vul[j-1].text] = {"title" : vul[j].text}
 
-			for i in soup_detail.find_all('ul')[2]:
-				spl = i.text.split(": ")
-				if len(spl)==2:
+			try:
+				for i in soup_detail.find_all('ul')[2]:
+					spl = i.text.split(": ")
+					#print(spl)
+					
+					p = re.compile('CVSS*')
+					m = p.match(spl[0])
+					
+					if m:
+						continue
+
+					#print(spl)
 					li[vul[j-1].text][spl[0]] = spl[1]
+			except:
+				try:
+					spl = soup_detail.find_all('li',class_='footnote')[0].text.split(". ")[1].split(",")[0].split(": ")
+					#print(spl)
+					li[vul[j-1].text]["vulnerability"] = spl
+				except:
+					pass
 
 js = json.dumps(li, indent=4)
 f = open("ics-cert-vulnerability.json",'w')
